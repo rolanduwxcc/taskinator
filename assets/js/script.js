c4e6c01f17edc69c3662484d4b24161d30ff7ef6
rolanduwxcc <rolanduwxcc@gmail.com>
2021-01-02 09:03:17 -0600

    refactored task function, created object to pass around

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,34 +5,37 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
     event.preventDefault();
-
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    //package data into an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput,
+    };
+
+    //send object as an arguement
+    createTaskEl(taskDataObj);
+
+};
+
+var createTaskEl = function(taskDataObj) {
     //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
     //create div element to hold task infor and add to list item vs li element
-    //AND give taskInfoEl a class name
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-
-    //add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>"; 
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>"; 
+    
     listItemEl.appendChild(taskInfoEl);
-
-    // var taskItemEl = document.createElement("li");
-    // taskItemEl.textContent = taskNameInput;
-    // taskItemEl.className = "task-item";
-    // tasksToDoEl.appendChild(taskItemEl);
 
     // add entire list itme to list
     tasksToDoEl.appendChild(listItemEl);
-    // console.dir(listItemEl);
+    // console.dir(listItemEl);    
 };
 
-// buttonEl.addEventListener("click",createTaskHandler);
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
