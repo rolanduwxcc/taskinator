fa99c2dd9cc1f95759331d7e4ec517ba73d544fe
rolanduwxcc <rolanduwxcc@gmail.com>
2021-01-01 22:34:53 -0600

    made form accept a submit click or Enter on keyboard to add task, added code to prevent auto refresh of page on task submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,19 @@
 //-------------------------------------------------------------VARIABLES
-var buttonEl = document.querySelector("#save-task");
+// var buttonEl = document.querySelector("#save-task");
+
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+
+var createTaskHandler = function(event) {
+    event.preventDefault();
+    // console.log(event); use this to get info on the event
+    
     var taskItemEl = document.createElement("li");
     taskItemEl.textContent = "This is a new task!";
     taskItemEl.className = "task-item";
     tasksToDoEl.appendChild(taskItemEl);
 };
 
-buttonEl.addEventListener("click",createTaskHandler);
+// buttonEl.addEventListener("click",createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
