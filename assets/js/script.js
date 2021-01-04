f0a70a3056ae94585ea3b58c790ac5cdf7a73abf
rolanduwxcc <rolanduwxcc@gmail.com>
2021-01-03 21:53:19 -0600

    Edit button now puts task info into form and Add button to say Save Task

--- assets/js/script.js
+++ assets/js/script.js
@@ -101,11 +101,26 @@ var createTaskActions = function(taskId) {
 var taskButtonHandler = function(event) {
     // console.log(event.target); //logs what object triggered event
 
-    if (event.target.matches(".delete-btn")) {
+    if (event.target.matches(".edit-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        editTask(taskId);
+    }
+    else if (event.target.matches(".delete-btn")) {
         var taskId = event.target.getAttribute("data-task-id");
         deleteTask(taskId);
     }
+};
 
+var editTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+
+    document.querySelector("input[name='task-name']").value = taskName;
+    document.querySelector("select[name='task-type']").value = taskType;
+    document.querySelector("#save-task").textContent = "Save Task";
+
+    formEl.setAttribute("data-task-id", taskId);
 };
 
 var deleteTask = function(taskId) {
