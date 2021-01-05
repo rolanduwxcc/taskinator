c78fcc632efb3d740865b6c1a78b6d476d29e844
rolanduwxcc <rolanduwxcc@gmail.com>
2021-01-04 21:59:55 -0600

    added more vents and completed the dragging portion

--- assets/js/script.js
+++ assets/js/script.js
@@ -185,7 +185,8 @@ var dropZoneDragHandler = function(event){
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
         event.preventDefault();
-        console.dir(taskListEl);
+        // console.dir(taskListEl);
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
 };
 
@@ -207,8 +208,17 @@ var dropTaskHandler = function(event) {
         statusSelectEl.selectedIndex = 2;
     }
 
+    dropZoneEl.removeAttribute("style");
+
     dropZoneEl.appendChild(draggableElement);
 
+};
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
 };
 
 //---------------------------------------------------------------EVENT LISTENERS
@@ -218,3 +228,4 @@ pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover",dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
